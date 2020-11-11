e9952e07614af65d4e8912d807570ab31237cb8a
FabianoRF <freis884@gmail.com>
2020-11-11 20:50:39 -0300

    challenge completed

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkUser = await this.customersRepository.findByEmail(email);
+
+    if (checkUser) {
+      throw new AppError('Consumer already exists');
+    }
+
+    const customer = await this.customersRepository.create({ email, name });
+
+    return customer;
   }
 }
 
